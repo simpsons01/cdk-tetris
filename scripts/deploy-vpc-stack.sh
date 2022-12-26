80c7989c1c0d7115d59d30e1ec8a3affb651c7c0
simpsosns01 <ilikekobe01@gmail.com>
2022-12-26 17:43:49 +0800

    private service 使用aws vpc endpoint 拉docker image

--- scripts/deploy-vpc-stack.sh
+++ scripts/deploy-vpc-stack.sh
@@ -4,4 +4,5 @@ STACK_NAME=TetrisVpc
 
 FILE_PATH=cloudformation/vpc.json
 
-aws cloudformation deploy --stack-name $STACK_NAME --template-body file://$FILE_PATH
+aws cloudformation deploy --stack-name $STACK_NAME --template-file $FILE_PATH
+
