b499d7798353d0dca152d328a7e04fc5573e94d9
simpsons01 <ilikekobe01@gmail.com>
2023-01-05 01:19:19 +0800

    connect service 使用0.04版

--- cdk/backend/lib/stacks.ts
+++ cdk/backend/lib/stacks.ts
@@ -104,7 +104,7 @@ export default class extends cdk.Stack {
       vpc,
       container: {
         name: connectServiceContainerName,
-        imageTag: `${connectServiceContainerName}_v0.03`,
+        imageTag: `${connectServiceContainerName}_v0.04`,
         taskDef: {
           memoryLimitMiB: 512,
           cpu: 256,
